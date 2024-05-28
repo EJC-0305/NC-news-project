ac7655a7b2fb752a799558bd4f4efcddf7e4f7ad
EJC-0305 <emmacliffart@gmail.com>
2024-05-28 15:50:03 +0100

    Creates controller and model files for articles and completes task 4 - GET article by id

--- db/app.js
+++ db/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics.controllers');
 const { getEndpoints } = require('./controllers/api.controllers');
+const { getArticleById } = require('./controllers/articles.controllers');
 
 const app = express();
 
 app.get('/api', getEndpoints);
 
 app.get('/api/topics', getTopics);
+
+app.get('/api/articles/:article_id', getArticleById);
+
+app.use((err, req, res, next) => {
+    if(err.msg){
+        res.status(err.status).send({ msg: err.msg })
+    } else {
+        next(err);
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(400).send({ msg: "Bad request" })
+  })
 
 module.exports = app;
---
+++ db/models/articles.models.js
@@ -0,0 +1,10 @@
+const db = require('../connection.js')
+
+exports.fetchArticleById = (article_id) => {
+    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id]).then((result) => {
+        if(result.rows.length === 0){
+            return Promise.reject({ status: 404, msg: "Article does not exist" })
+        }
+        return result.rows[0];
+    });
+}
