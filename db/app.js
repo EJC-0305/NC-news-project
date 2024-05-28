f389964a4271a3b32d9ac985f78594d7c5855162
EJC-0305 <emmacliffart@gmail.com>
2024-05-28 16:04:26 +0100

    Completes error handling for non-existing endpoints

--- db/app.js
+++ db/app.js
@@ -11,6 +11,10 @@ app.get('/api/topics', getTopics);
 
 app.get('/api/articles/:article_id', getArticleById);
 
+app.all('*', (req, res) => {
+    res.status(404).send({msg: "Not found"})
+      })
+
 app.use((err, req, res, next) => {
     if(err.msg){
         res.status(err.status).send({ msg: err.msg })
