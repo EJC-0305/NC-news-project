96ba42bba2a7a04176a6473138dc1647263a60e0
EJC-0305 <emmacliffart@gmail.com>
2024-05-29 17:20:15 +0100

    Completes more error handling following feedback

--- db/app.js
+++ db/app.js
@@ -36,7 +36,11 @@ app.use((err, req, res, next) => {
 
 app.use((err, req, res, next) => {
     if(err.code === '23503') {
-        res.status(404).send({ msg: "Article does not exist" })
+        let msg = "Article does not exist";
+        if(err.detail.includes('author')){
+            msg = 'User does not exist';
+        }
+        res.status(404).send({ msg })
     } else {
         res.status(400).send({ msg: "Bad request" })
     }
