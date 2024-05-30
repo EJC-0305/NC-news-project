62f9d8317015e61c935fe17cc78d81aed33d3271
EJC-0305 <emmacliffart@gmail.com>
2024-05-30 09:53:38 +0100

    Completes task 9 - code and error handling for DELETE /api/comments/comment_id endpoint

--- db/controllers/comments.controllers.js
+++ db/controllers/comments.controllers.js
@@ -1,5 +1,5 @@
 const { fetchArticleById } = require("../models/articles.models")
-const { fetchComments, createComment } = require("../models/comments.models")
+const { fetchComments, createComment, removeComment } = require("../models/comments.models")
 
 exports.getComments = (req, res, next) => {
     Promise.all([fetchComments(req.params.article_id), fetchArticleById(req.params.article_id)])
@@ -16,6 +16,19 @@ exports.postComment = (req, res, next) => {
     createComment(req.body, req.params.article_id)
     .then((new_comment) => {
         res.status(201).send({ new_comment })
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+
+exports.deleteComment = (req, res, next) => {
+    removeComment(req.params.comment_id)
+    .then((data) => {
+        if(!data.rowCount) {
+            next({ status: 404, msg: "Comment does not exist" });
+        }
+        res.status(204).send();
     })
     .catch((err) => {
         next(err)
