0ab7f5199c32d6b0f9a9e72681b093c569dfb73b
EJC-0305 <emmacliffart@gmail.com>
2024-05-31 15:51:30 +0100

    Completes code and error handling for /api/users/:username endpoint

--- db/models/users.models.js
+++ db/models/users.models.js
@@ -6,3 +6,13 @@ exports.fetchUsers = () => {
         return result.rows;
     })
 }
+
+exports.fetchUserByUsername = (username) => {
+    return db.query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then((result) => {
+        if(!result.rows.length){
+            return Promise.reject({ status: 404, msg: "User does not exist" })
+        }
+        return result.rows[0];
+    })
+}
