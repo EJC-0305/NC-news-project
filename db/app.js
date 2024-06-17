a5ae14c3a0888ec2e7cd9800680ea5701bc61e7b
EJC-0305 <emmacliffart@gmail.com>
2024-06-17 10:52:43 +0100

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const apiRouter = require('./routers/api-router.js');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
